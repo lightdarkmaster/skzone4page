e79e43acce5923fc2389b25094922f1a92eb1752
lightdarkmaster <79690406+lightdarkmaster@users.noreply.github.com>
2024-05-04 16:06:32 +0800

    Add Some changes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
-import React from 'react'
-import NavBar from './NavBar'
+import React from 'react';
+import NavBar from './NavBar';
+import Home from './Home';
 
 function App() {
   return (
     <div className="App">
       <NavBar/>
+      <Home/>
     </div>
   )
 }
 
-export default App
+export default App;
