04db3f91d9ab1bc8b265d2397636d92f43948990
lightdarkmaster <79690406+lightdarkmaster@users.noreply.github.com>
2024-05-04 08:14:50 +0800

    Add logo

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import NavBar from './NavBar'
 
 function App() {
   return (
     <div className="App">
-      //ap will be displayed here..
+      <NavBar/>
     </div>
   )
 }
---
+++ src/NavBar.js
@@ -0,0 +1,71 @@
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import './NavBar.css';
+import logo from "./assets/SK.png";
+
+function NavBar() {
+  return (
+    <div className="NavBar">
+      <nav class="navbar navbar-expand-lg bg-body-tertiary">
+        <div class="container-fluid">
+          <a class="navbar-brand" href="#">
+            <img className='logo' src={logo}/>
+          </a>
+          <button
+            class="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+              <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="#">
+                  Home
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">
+                  About
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">
+                  Projects
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">
+                  Events
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">
+                  News
+                </a>
+              </li>
+            </ul>
+            <form class="d-flex" role="search">
+              <input
+                class="form-control me-2"
+                type="search"
+                placeholder="Search"
+                aria-label="Search"
+              />
+              <button class="btn btn-outline-success" type="submit">
+                Search
+              </button>
+            </form>
+          </div>
+        </div>
+      </nav>
+    </div>
+  );
+}
+
+export default NavBar;
